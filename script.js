e46e15cf7d077388e35a15d4b32482e551b689cd
Ntaganira kwitonda theos <kwitondatheos@gmail.com>
2025-10-19 22:48:05 +0200

    Implement light and dark theme switching
    Added light and dark theme functionality with enhanced styling for various elements including buttons, headers, and main sections.

--- script.js
+++ script.js
@@ -1,20 +1,102 @@
-let darkTheme = document.querySelector(".dark")
-let lightTheme = document.querySelector(".light")
-let body = document.querySelector("body")
-let boxes = document.querySelectorAll(".boxes")
+const darkTheme = document.querySelector(".dark")
+const lightTheme = document.querySelector(".light")
+const body = document.querySelector("body")
+const boxes = document.querySelectorAll(".boxes")
+const removeButtons = document.querySelectorAll(".remove")
+const header = document.querySelector("header")
+const main = document.querySelector("main")
+const filterInput = document.querySelectorAll(".filter-buttons")
+const boxDescriptionP = document.querySelectorAll("p")
+
 
 // switch themes
-darkTheme.addEventListener("click", () => {
+if (lightTheme) {
+    lightTheme.addEventListener("click", () => {
     body.classList.add("light-mode")
+
     boxes.forEach(box => {
-        box.classList.add("light-mode")
+        box.classList.add("boxes-light-mode")
+        
     })
+    // buttons
+    removeButtons.forEach(button => {
+        button.style.backgroundColor = "white"
+        button.style.color = "black"
+        button.style.border = "1px solid rgb(216, 216, 216)"
+        button.style.transition = "background-color 0.3s ease"
+        button.addEventListener("mouseover", () => {
+            button.style.backgroundColor = "hsl(0, 0%, 93%)"
+        })
+        button.addEventListener("mouseout", () => {
+            button.style.backgroundColor = "white"
+        })
+        
+    })
+    // filter input
+    filterInput.forEach(input => {
+        
+        input.classList.add("filter-buttons-light")
+    })
+    // box description paragraph
+    boxDescriptionP.forEach(p => {
+        p.style.color = "#000"
+    })
+    // header
+    
+    header.classList.add("header-light-mode")
+    
+    //main section
+    main.classList.add("main-light-mode")
+    // icon toggle
+    darkTheme.style.display = "block"
+    lightTheme.style.display = "none"
+})
+}
+if (darkTheme) {
+    darkTheme.addEventListener("click", () => {
+    body.classList.remove("light-mode") 
+    boxes.forEach(box => {
+        box.classList.remove("boxes-light-mode")
+    })
+    // buttons
+    removeButtons.forEach(button => {  
+        button.style.backgroundColor = ""
+        button.style.color = ""
+        button.style.border = ""
+    })
+    // filter input
+    filterInput.forEach(input => {
+        input.classList.remove("filter-buttons-light")
+    })
+    // box description paragraph
+    boxDescriptionP.forEach(p => {
+        p.style.color = ""
+    })
+    // header
+    header.classList.remove("header-light-mode")
+    // main section
+    main.classList.remove("main-light-mode")
+    // icon toggle
     darkTheme.style.display = "none"
     lightTheme.style.display = "block"
 })
+}
 
-lightTheme.addEventListener("click", () => {
-    body.classList.remove("light-mode")
-    lightTheme.style.display = "none"
-    darkTheme.style.display = "block"
+
+// classes for extension objects
+class extension{
+    constructor(image, name, description){
+        this.image = image
+        this.name = name
+        this.description = description
+    }
+    
+
+}
+const extensionsList = []
+
+// sample extensions
+const ext1 = new extension("assets/images/logo-devlens.svg","Devlens","Quickly inspect page layouts and <br>visualize element boundaries")
+boxes.forEach(box => {
+    box.style.backgroundColor = "hsl(226, 25%, 17%)"
 })
